98fa83a4ee39bf575000b64afea795b4042adc01
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-15 15:47:00 -0300

    criado alunos para testar funcionalidades da classe aluno

--- src/projetoAluno/Principal.java
+++ src/projetoAluno/Principal.java
@@ -3,7 +3,14 @@ package projetoAluno;
 public class Principal {
 
 	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno(5, "Josefino");
+		a2.setAnoNasc(2003);
+		a2.setEmail("josefino@email.com");
+		a2.setAtivo(true);
 		
+		System.out.println(a1);
+		System.out.println(a2);
 	}
 
 }
