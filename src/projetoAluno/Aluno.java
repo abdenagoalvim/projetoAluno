71ef04a61e8ae321dfef88f47cdc56a19d57ac71
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-15 15:46:03 -0300

    criado os construtores, setters, getters e toString da classe Aluno

--- src/projetoAluno/Aluno.java
+++ src/projetoAluno/Aluno.java
@@ -6,5 +6,54 @@ public class Aluno {
 	private int anoNasc;
 	private String email;
 	private boolean ativo;
+	
+	public Aluno() {
+	}
+	public Aluno(int matricula, String nome) {
+		this.matricula = matricula;
+		this.nome = nome;
+	}
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAnoNasc() {
+		return anoNasc;
+	}
+	public void setAnoNasc(int anoNasc) {
+		this.anoNasc = anoNasc;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
 
+	@Override
+	public String toString() {
+		return "Aluno ["
+				+ "\n\tmatricula = " + matricula 
+				+ ",\n\tnome = " + nome 
+				+ ",\n\tanoNasc = " + anoNasc 
+				+ ",\n\temail = " + email
+				+ ",\n\tativo = " + (ativo?"Sim":"Não")
+				+ "\n]";
+	}
+	
 }
