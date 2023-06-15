ec9c1b561d6c29b6ae946d9279ba95632964bc0c
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-15 09:18:49 -0300

    incluido atributos das classes Aluno e Turma

--- src/projetoAluno/Aluno.java
+++ src/projetoAluno/Aluno.java
@@ -1,5 +1,10 @@
 package projetoAluno;
 
 public class Aluno {
+	private int matricula;
+	private String nome;
+	private int anoNasc;
+	private String email;
+	private boolean ativo;
 
 }
--- src/projetoAluno/Turma.java
+++ src/projetoAluno/Turma.java
@@ -1,5 +1,11 @@
 package projetoAluno;
 
+import java.util.ArrayList;
+
 public class Turma {
+	private int codigo;
+	private int serie;
+	private String turno;
+	private ArrayList<Aluno> listaAlunos = new ArrayList<>();
 
 }
