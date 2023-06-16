00f7a2afd0a86a8cd62c4dde771a3234ae0849cc
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-16 16:18:35 -0300

    incluído métodos: contrutores, setters, getters e toString

--- src/projetoAluno/Turma.java
+++ src/projetoAluno/Turma.java
@@ -8,4 +8,43 @@ public class Turma {
 	private String turno;
 	private ArrayList<Aluno> listaAlunos = new ArrayList<>();
 
+	public Turma() {
+	}
+
+	public Turma(int codigo, int serie, String turno) {
+		this.codigo = codigo;
+		this.serie = serie;
+		this.turno = turno;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public int getSerie() {
+		return serie;
+	}
+
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	@Override
+	public String toString() {
+		return "Turma [codigo = " + codigo + ", serie = " + serie + ", turno = " + turno + "]"
+				+ "\nLista de Alunos:\n" + listaAlunos;
+	}
+
 }
