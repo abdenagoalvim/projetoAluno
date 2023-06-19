d6bbaf3c01c02252d596a39406c7f77a23f4c1d4
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-19 15:35:46 -0300

    incluído alunos e adicionados a turmas para teste das alterações

--- src/projetoAluno/Principal.java
+++ src/projetoAluno/Principal.java
@@ -16,7 +16,20 @@ public class Principal {
 		t1.setCodigo(713);
 		t1.setSerie(3);
 		t1.setTurno("Tarde");
+		t1.adicionarAluno(a1);
+		t1.adicionarAluno(a2);
+		
+		Aluno a3 = new Aluno(7, "Marieta");
+		a3.setAnoNasc(2010);
+		a3.ativar();
+		Aluno a4 = new Aluno(7, "Orozimbo");
+		a4.setAnoNasc(1975);
+		a4.ativar();
+		
 		Turma t2 = new Turma(725, 3, "Manhã");
+		t2.adicionarAluno(a3);
+		t2.adicionarAluno(a4);
+		
 		
 		System.out.println(t1);
 		System.out.println(t2);
